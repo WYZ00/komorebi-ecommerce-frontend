7fe8d20c0ac2dabb6a5585d6a373d23479793fc2
WYZ00 <thanthtettin@gmail.com>
2023-07-28 23:42:52 +0630

    footer

--- src/components/routes/root.tsx
+++ src/components/routes/root.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Inspx from "inspx";
 import Header from "../header";
+import Footer from "../footer";
 
 const Root = () => {
   return (
     <Inspx>
       <Header />
       <Outlet />
+      <Footer />
     </Inspx>
   );
 };
